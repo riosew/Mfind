253e25d0b9a721271a22757c877ef743b8f428ae
riosew <141943816+riosew@users.noreply.github.com>
2024-01-12 12:26:08 -0600

    Create inicio.java

---
+++ inicio.java
@@ -0,0 +1,15 @@
+package biomarker1;
+
+/**
+ * MFind Sofware Rios-Willars
+ * This code searches for microsatellites in a multifasta file
+ * uses a sliding window and a parallel process for the search
+ */
+
+public class Inicio {
+
+    public static void main(String[] args) {          //The begining
+        panel panel = new panel();
+        panel.setVisible(true);
+    }
+}
